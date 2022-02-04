86b3237a73c39961634744dfcb9eadb07ef21dfd
patpatforeman <patrickforeman123@gmail.com>
2022-02-03 21:49:56 -0500

    Edit Commit

--- Foreman Code/functional_pi.py
+++ Foreman Code/functional_pi.py
@@ -1,10 +1,47 @@
 import math
 
-def calculate_pi():
+def calculate_pi(de):
+    """
+    Returns a value for 𝜋 within a certain error bound.
 
+    :param de: Desired Error Limits for Generated Pi Value
+    """
+    # Set Initial Values
+    a = 1
+    b = 1 / (2 ** (1 / 2))
+    t = 1 / 4
+    p = 1
+    approx = 0
+    loop = range(10000)
+
+    # Create a loop to iterate through ten times
+    for i in loop:
+        a_n = ((a + b) / 2)
+        b_n = ((b * a) ** (1 / 2))
+        t_n = t - p * ((a - a_n) ** 2)
+        p_n = (2 * p)
+
+        # This Section gives me my pi approximation and the percent difference between my pi
+        # and Python's pi
+        approx = ((a_n + b_n) ** 2) / (4 * t_n)
+        error = (abs((approx - math.pi) / math.pi)) * 100
+
+        # This simply appends the initial values to the final values to increase accuracy through the loop
+        a = a_n
+        b = b_n
+        t = t_n
+        p = p_n
+
+        # Breakout Condition
+        if error > de or error == de:
+            continue
+        else:
+            break
+    return approx
 
 # main (body) here to call your function. Do not modify below this line
 desired_error = 1E-10
+
 
 approximation = calculate_pi(desired_error)
 

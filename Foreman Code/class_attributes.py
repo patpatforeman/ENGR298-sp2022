5d63b10af71af907461653937a2f4e4ea0bd60d4
patpatforeman <patrickforeman123@gmail.com>
2022-03-10 19:52:15 -0500

    beans

--- Foreman Code/class_attributes.py
+++ Foreman Code/class_attributes.py
@@ -27,31 +27,62 @@ class Vector:
         return "<" + str(self.x) + "," + str(self.y) + "," + str(self.z) + ">"
 
 
-# Main program begins here ...
+def compute_dot_product(v1, v2):
+    """
+    Given two vectors (V1 and V2), compute the dot product
+    :param v1: A three element vector with x, y, and z components <v1.x, v1.y, v1.z>
+    :param v2: A three element vector with x, y, and z components <v2.x, v2.y, v2.z>
+    :return: Dot product of vector 1 and vector 2
+    """
 
-# create two simple vectors
-a = Vector(3, 1, -2)
-b = Vector(9, 2, 4)
+    # variable to hold the dot product result
+    dot_product = (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z)
 
-# treat the vectors as point as compute the distance between (should be 8.54)
-distance = sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2 + (b.z - a.z) ** 2)
-print("Distance between ", a, " and ", b, " is ", distance)
+    # do not modify this line
+    return dot_product
 
-# treat vectors as vectors and compute the dot product (should be 21)
-dot_product = (b.x * a.x) + (b.y * a.y) + (b.z * a.z)
-print("Dot product between ", a, " and ", b, " is ", dot_product)
 
-# generate two random vectors and perform the same operations
-(x, y, z) = random.sample(range(10), 3)
-v1 = Vector(x, y, z)
+def compute_distance(v1, v2):
+    """
+    Given two vectors (V1 and V2), compute the distance between the vectors
+    :param v1: A three element vector with x, y, and z components <v1.x, v1.y, v1.z>
+    :param v2: A three element vector with x, y, and z components <v2.x, v2.y, v2.z>
+    :return: Distance between vector 1 and vector 2
+    """
 
-(x, y, z) = random.sample(range(10), 3)
-v2 = Vector(x, y, z)
+    # variable to hold the calculated distance
+    distance = sqrt((v2.x - v1.x) ** 2 + (v2.y - v1.y) ** 2 + (v2.z - v1.z) ** 2)
 
-# treat the vectors as point as compute the distance between (check result by hand)
-distance = sqrt((v2.x - v1.x) ** 2 + (v2.y - v1.y) ** 2 + (v2.z - v1.z) ** 2)
-print("Distance between ", v1, " and ", v2, " is ", distance)
 
-# treat vectors as vectors and compute the dot product (check result by hand)
-dot_product = (v2.x * v1.x) + (v2.y * v1.y) + (v2.z * v1.z)
-print("Dot product between ", v1, " and ", v2, " is ", dot_product)
+    # do not modify this line
+    return distance
+
+
+### Main program begins here... ###
+if __name__ == "__main__":
+    # create two simple vectors
+    a = Vector(3, 1, -2)
+    b = Vector(9, 2, 4)
+
+    # treat the vectors as point as compute the distance between (should be 8.54)
+    distance = compute_distance(a, b)
+    print("Distance between ", a, " and ", b, " is ", distance)
+
+    # treat vectors as vectors and compute the dot product (should be 21)
+    dot_product = compute_dot_product(a, b)
+    print("Dot product between ", a, " and ", b, " is ", dot_product)
+
+    # generate two random vectors and perform the same operations
+    (x, y, z) = random.sample(range(10), 3)
+    v1 = Vector(x, y, z)
+
+    (x, y, z) = random.sample(range(10), 3)
+    v2 = Vector(x, y, z)
+
+    # treat the vectors as point as compute the distance between (check result by hand)
+    distance = compute_distance(v1, v2)
+    print("Distance between ", v1, " and ", v2, " is ", distance)
+
+    # treat vectors as vectors and compute the dot product (check result by hand)
+    dot_product = compute_dot_product(v1, v2)
+    print("Dot product between ", v1, " and ", v2, " is ", dot_product)
